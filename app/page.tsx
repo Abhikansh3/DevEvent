2fb8e8647406f95a21bfc174f7a8cc782416db98
Abhikansh Mittal <abhikansh33@gmail.com>
2026-02-17 12:53:22 +0530

    added btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import ExploreBtn from "@/components/ExploreBtn"
+
 const page = () => {
   return (
     <section>
       <h1 className="text-center">The hub for Every Dev <br /> Event You Can't Miss   </h1>
-      <p className="text-center mt-5">Hackathons, Meetups ans Conferences, All in One Place </p>
+      <p className="text-center mt-4">Hackathons, Meetups ans Conferences, All in One Place </p>
+      <ExploreBtn />
     </section>
-
   )
 
 }
